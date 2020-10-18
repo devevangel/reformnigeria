56bccf20255e721da634e1704e1424cb28b1c64a
devevangel <dev.evangel@gmail.com>
2020-10-18 20:06:32 +0100

    Updates

---
+++ precache-manifest.f975590beda76a2cc46260d22db10e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f873bd3f0f4dea81f4e364e5090d29c",
+    "url": "/reformnigeria/index.html"
+  },
+  {
+    "revision": "9fc5d10dc6595c0b21c0",
+    "url": "/reformnigeria/static/css/main.c122062b.chunk.css"
+  },
+  {
+    "revision": "170301849e89d968e6eb",
+    "url": "/reformnigeria/static/js/2.633241eb.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/reformnigeria/static/js/2.633241eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc5d10dc6595c0b21c0",
+    "url": "/reformnigeria/static/js/main.cdba2719.chunk.js"
+  },
+  {
+    "revision": "6b51dfa98095e79ac059",
+    "url": "/reformnigeria/static/js/runtime-main.5c2a9691.js"
+  }
+]);
